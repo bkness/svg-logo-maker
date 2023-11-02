7c5c008bddb503aa1906b5325b26d23e5a2d0d8f
bkness <kbrandon863@gmail.com>
2023-11-01 21:42:44 -0700

    resized my logo shapes so they are 300 by 200 per readme requet, app is full functional with comments now, just need to add a readme

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+// using fs and inquirer and each constructor class from my shapes.js
 const inquirer = require('inquirer');
 const fs = require('fs').promises;
 const { Triangle, Circle, Square } = require('./lib/shapes.js');
 
+// questions array that will ask the user a series of questions in the terminal
 const questions = [
     {
         type: 'list',
@@ -26,27 +28,36 @@ const questions = [
     }
 ]
 
+// a function that takes data and uses an operator selctor to get the appropriate shape
 function generateSvg(data) {
     if (data.shape === 'Triangle') {
         const shape = new Triangle(data.textColor, data.color, data.text);
         return shape.render();
     } else if (data.shape === 'Circle') {
         const shape = new Circle(data.textColor, data.color, data.text);
-        return shape.render() 
+        return shape.render()
     } else if (data.shape === 'Square') {
         const shape = new Square(data.textColor, data.color, data.text);
-        return shape.render(); // calling a render that doesnt exist
-
-    // more shapes 
-}}
+        return shape.render();
+    }
+}
 // making a render for each shape each needs a slightly diff attribute 
-function init() { 
-    inquirer.prompt(questions).then(data => {
-    fs.writeFile('./examples/shape.svg', generateSvg(data)).then(err => {
-        err ? console.error(err) : console.log('Logo was successful')
-    })
-    })
+function init() {
+    try {
+        inquirer.prompt(questions).then(data => {
+            fs.writeFile('./examples/shape.svg', generateSvg(data)).then(() => {
+                console.log('Logo was successful');
+            }).catch(err => {
+                throw err; // if an error happens while writing the file throw it
+            });
+        }).catch(err => {
+            throw err; // if an error happens during prompt throw it
+        });
+    } catch (err) {
+        console.error(err); // handles the error here
+    }
 };
+
 
 // Function call to initialize app
 init();
